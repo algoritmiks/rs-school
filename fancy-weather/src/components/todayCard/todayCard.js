825ec2c2d615db35c52a5fd28a1528b9fae81fa2
Aleksey Osipov <alexey11rus@gmail.com>
2020-06-15 23:56:41 +0300

    refactor: todayCard

--- fancy-weather/src/components/todayCard/todayCard.js
+++ fancy-weather/src/components/todayCard/todayCard.js
@@ -1,30 +1,18 @@
 import React from 'react';
 
 function TodayCard (props) {
-
-  let weather = {};
-  
-  if (props.state.weather) {
-    let todayWeather = props.state.weather.data[0]; 
-    let weatherCode = todayWeather.weather.code;
-    let feelsTemp = Math.round( (todayWeather.app_min_temp + todayWeather.app_max_temp) /2 );
-    let lang = props.state.localisations[`${props.state.language}`];
-    weather.pictureToday = `https://www.weatherbit.io/static/img/icons/${todayWeather.weather.icon}.png`;
-    weather.tempToday = `${Math.round(todayWeather.temp)}°`;
-    weather.feelTemp = `${lang.feel} ${feelsTemp}°`;
-    weather.description = lang.weather[weatherCode];
-  }
-
   return (
     <div className="weather-today">
       <div className="city"> {props.state.location}</div>
       <div className="country"> {props.state.country}</div>
       <div className="time"></div>
       <div className="weather">
-        <div className="temperature"> {weather.tempToday} </div>
-        <img className="weather-icon-today" src={weather.pictureToday} alt="weather"></img>
-        <div>{weather.description}</div>
-        <div>{weather.feelTemp}</div>
+        <div className="temperature"> {props.weather.temp} </div>
+        <img className="weather-icon-today" src={props.weather.picture} alt="weather"></img>
+        <div>{props.weather.descr}</div>
+        <div>{props.weather.feel}</div>
+        <div>{props.weather.humidity}</div>
+        <div>{props.weather.wind}</div>
       </div>
       
     </div>
--- fancy-weather/src/components/weather/Weather.js
+++ fancy-weather/src/components/weather/Weather.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import TodayCard from '../todayCard/todayCard';
 
-export class Weather extends React.Component {
-  
-  render() {
-    return (
-      <div className="weather-wrapper">
-        <TodayCard state = { this.props.state }/>
-      </div>
-    )
+function Weather (props) {
+  const todayWeather = {};
+
+  if (props.state.weather) {
+    const lang = props.state.localisations[`${props.state.language}`];
+    const todayWeatherData = props.state.weather.data[0]; 
+    const todayWeatherCode = todayWeatherData.weather.code;
+    const todayFeelsTemp = Math.round( (todayWeatherData.app_min_temp + todayWeatherData.app_max_temp) / 2 );
+    
+    todayWeather.picture = `https://www.weatherbit.io/static/img/icons/${todayWeatherData.weather.icon}.png`;
+    todayWeather.temp = `${Math.round(todayWeatherData.temp)}°`;
+    todayWeather.feel = `${lang.feel} ${todayFeelsTemp}°`;
+    todayWeather.descr = lang.weather[todayWeatherCode];
+    todayWeather.humidity = `${lang.humidity} ${todayWeatherData.rh}%`;
+    todayWeather.wind = `${lang.wind} ${Math.round(todayWeatherData.wind_spd)} m/s`
   }
+
+
+  return (
+    <div className="weather-wrapper">
+      <TodayCard state = { props.state } weather = { todayWeather }/>
+    </div>
+  )
+  
 }
 
 export default Weather;
